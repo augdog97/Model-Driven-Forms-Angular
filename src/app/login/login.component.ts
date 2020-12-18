17cd0989b3fa1b2fd909ec9a95e4be7e0dfce2cb
augdog97 <augdog@me.com>
2020-12-17 20:12:49 -0500

    Added password validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,22 @@
 
 /*
-  1. Imported formgroup, formcontrol, and validators from Angular Core
-  2. created a new property called form and initialize it with "new FormGroup"
-    - We pass in two FormControl objects, username and password into FormGroup()
+  1. Imported formgroup, FormBuilder, and validators from Angular Core
+    - FormBuilder class allows to declare FormControl and FormGroup objects more compactly.
+  2. Using dependency injection to get an instance of FormBuilder, fb. FormBuilder has a method group which takes in a shortened code for new FormControl. 
+      - The group method returns a FormGroup object as a result and we store it in "form".
+      - FormBuilder is like syntactic sugar that shortens the new FormGroup() and new FormControl() code that can build up in larger forms.
       - First parameter in FormControl is optional. It is useful when we display existing data in the control for the user to edit. We first retreive the existing value from the server and populate the control with it.
       - Second parameter is the Validator function. Validators provide a set of validators like required, minlength, maxlength. 
-  3. Creating the login method that is binded to ngSubmit
+  3. Import the PasswordValidator and apply it to "password". Because we have more than one validator on "password" form control, we need to compose the multiple validators by calling Validators.compose method. 
+    - The compost method takes in an array of validators. 
+  4. Creating the login method that is binded to ngSubmit
 */
 
 
 import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+
+import {PasswordValidator} from '../passwordValidator';
 
 @Component({
   selector: 'login',
@@ -18,11 +24,13 @@ import {FormGroup, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent   {
-
-  form = new FormGroup({
-    username: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required)
-  });
+  form: FormGroup;
+ constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      username:["", Validators.required],
+      password:["", Validators.compose([Validators.required, PasswordValidator.cannotContainSpace])]
+    })
+ }
 
   login() {
     console.log(this.form.value);
---
+++ src/app/passwordValidator.ts
@@ -0,0 +1,20 @@
+
+/**
+ * Declared a static method cannotContainSpace that takes in a FormControl object as an argument.
+ * We access the value string property of the formControl and check if there are spaces in it with the indexOf method. 
+ * If there are spaces in it, we return cannotContainSpace: true, and if there are no spaces, we return null.
+ */
+
+ import {FormControl} from '@angular/forms';
+
+ export class PasswordValidator {
+     static cannotContainSpace(formControl: FormControl) {
+         if(formControl.value.indexOf('')>= 0) {
+             return { cannotContainSpace: true };
+         }
+         else {
+             return null;
+         }
+         
+     }
+ }
